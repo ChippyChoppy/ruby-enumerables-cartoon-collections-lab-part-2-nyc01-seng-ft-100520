4926131b290645e5b352af9654e77ef42b244d07
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 02:07:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   mixed_calls.find. do |call| 
-   call = valid_calls
-  end   
+   call = valid_calls 
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 end

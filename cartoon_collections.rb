1983dd9d7b760c62e8ecf74d45e61bca3cdf6b2a
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 02:13:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ require 'pry'
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.find { |call| call = valid_calls }
+  planeteer_calls.find { |call| valid_calls.include? call}
  binding.pry
  
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 

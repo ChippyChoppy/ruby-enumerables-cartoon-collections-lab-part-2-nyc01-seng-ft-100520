59a66dfaf482bc6daf8d9cb4e4467a1ab6cc27b2
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 02:00:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   planeteer_calls.find do |call| 
-    if call = valid_calls[]
-      call
-    else
-      nil 
+    if call = valid_calls
     end
   end
 
